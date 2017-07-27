1d2636e0837cc251953fe884e9993dd2330d2e80
anant-k-singh <anant02sep@gmail.com>
2017-07-27 17:25:23 +0530

    Update AMBLE_DP_Spoj.cpp

--- AMBLE_DP_Spoj.cpp
+++ AMBLE_DP_Spoj.cpp
@@ -1,3 +1,4 @@
+// AKS
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,43 +14,29 @@ double dist(point p1, point p2){
 }
 
 double mindist(point* p, int n){
-    double p1_value = 0.0, p2_value = 0.0;
-    int p1_last = 0, p2_last = 0;
-    for(int i=1;i<n-1;++i){
-        double d1 = dist(p[i],p[p1_last]);
-        double d2 = dist(p[i],p[p2_last]);
-        if(d1 + p1_value < d2 + p2_value){
-            p1_value += d1;
-            p1_last = i; 
-        }
-        else{
-            p2_value += d2;
-            p2_last = i; 
-        }
+    double ans = 1000000001.0, ends[n][n-1];
+    for(int j=0;j<n-1;++j){
+        double m = 1000000002.0;
+        for(int x=0; x<j;++x)
+            m = min(m, ends[j][x]+dist(p[x],p[j+1]));
+        if(m>=1000000000.0) m=dist(p[0],p[1]);
+        ends[j+1][j] = m;
+        for(int i=j+2;i<n;++i)
+            ends[i][j] = ends[i-1][j] + dist(p[i],p[i-1]);
     }
-    double ans = p1_value + p2_value;
 
-    //visit destination on both paths
-    ans += dist(p[n-1],p[p1_last]);
-    ans += dist(p[n-1],p[p2_last]);
+    for(int j=0;j<n-1;++j)
+        ans = min(ans, ends[n-1][j] + dist(p[j],p[n-1]));
+
     return ans;
-}
-
-bool comp(point l, point r){
-    return l.x<r.x;
 }
 
 int main(){
     int n,x;
     cin>>n;
     point p[n];
-    for(int i=0;i<n;++i)
-        cin>>p[i].x>>p[i].y;
-    sort(p,p+n,comp);
-    if(n==1){
+    for(int i=0;i<n;++i)    cin>>p[i].x>>p[i].y;
+    if(n<=1)
         cout<<"0.00"<<endl;
-        return 0;
-    }
-    cout<<std::fixed<<setprecision(2)<<mindist(p,n)<<endl;
-    return 0;
+    else cout<<std::fixed<<setprecision(2)<<mindist(p,n)<<endl;
 }
